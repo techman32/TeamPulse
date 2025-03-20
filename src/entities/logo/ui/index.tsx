42459ddd905a70e508e4b818850c7e1fcbe465ff
techman32 <klochko977@gmail.com>
2025-03-20 14:05:25 +0300

    Добавил layout dashboard

---
+++ src/entities/logo/ui/index.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Logo() {
+  return (
+    <div className="flex justify-start p-4 border-b border-gray-200 shadow-xs fixed top-0 left-0 right-0 bg-white z-10">
+      <Link href="/dashboard">
+        <p className="text-xl font-semibold">TeamPulse</p>
+      </Link>
+    </div>
+  )
+}
