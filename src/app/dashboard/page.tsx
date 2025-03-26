03d6b07e1d9e7a755282cb86060c6eff14962a79
techman32 <klochko977@gmail.com>
2025-03-26 14:57:48 +0300

    Ссылка на документацию

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
 export default function DashboardPage() {
   return (
     <>
       <h1 className="font-bold text-4xl">Добро пожаловать!</h1>
-      <div>
+      <div className="flex flex-col gap-2">
         <p className="font-semibold text-xl">Здесь вы можете:</p>
-        <ul className="mt-2 flex flex-col gap-1">
+        <ul className="flex flex-col gap-1">
           <li>1. Создать шаблон теста или импортировать из CSV файла</li>
           <li>2. Создать пользователя или импортировать из CSV файла</li>
           <li>3. Создать теги и группы, по которым можно в будущем разбить вопросы и пользователей соответственно</li>
@@ -14,6 +16,13 @@ export default function DashboardPage() {
           </li>
           <li>5. В тесте указывать оценки для каждого вопроса, чтобы в дальнейшем было удобнее анализировать ответы</li>
         </ul>
+        <Link
+          href="https://docs.google.com/document/d/19CLw2jJrnhJ7QUFIxw1h1wNhupnVeeAuTmL7e5wpEl8/edit?tab=t.0"
+          target="_blank"
+          className="text-black underline-offset-2 underline hover:no-underline"
+        >
+          Полное руководство по использованию
+        </Link>
       </div>
     </>
   )
